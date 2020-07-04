d1cf695e17dd4d58691c417477c0d02dccfdb972
João Pedro <joao.brandao@uniriotec.br>
2020-07-04 12:30:34 -0300

    definição de variáveis sass e estruturação inicial dos componentes

---
+++ frontend/src/Content.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Content.scss';
+
+function Content() {
+    return (
+        <section className="Content">
+            a
+        </section>
+    );
+}
+
+export default Content;
---
+++ frontend/src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.scss';
+
+function Footer() {
+    return (
+        <footer className="Footer">
+            a
+        </footer>
+    );
+}
+
+export default Footer;
