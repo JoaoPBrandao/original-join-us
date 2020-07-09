183dd81c799d54d15ec2a02106504945c11634da
João Pedro <joao.brandao@uniriotec.br>
2020-07-09 11:31:24 -0300

    responsividade mobile header

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -2,16 +2,20 @@ import React from 'react';
 import logo from './images/logo.svg';
 import lupa from './images/lupa.svg';
 import carrinho from './images/carrinho.svg';
+import carrinhoMobile from './images/carrinhoMobile.svg';
 import './Header.scss';
 import Carrinho from './Carrinho';
 import { connect } from 'react-redux';
+import {ReactComponent as Barras} from "./images/barras.svg";
 
 class Header extends React.Component{
 
     constructor(props) {
         super(props);
         this.state = {
-            mostrarModal: false
+            mostrarModal: false,
+            menuAberto: false,
+            buscaAberta: false
         };
     }
 
@@ -21,6 +25,14 @@ class Header extends React.Component{
 
     fecharModal () {
         this.setState({ mostrarModal: false });
+    }
+
+    alternarMenu() {
+        this.setState({menuAberto: !this.state.menuAberto})
+    }
+
+    alternarBusca() {
+        this.setState({buscaAberta: !this.state.buscaAberta})
     }
 
     render() {
@@ -37,7 +49,7 @@ class Header extends React.Component{
                                 <span className="separador">|</span>
                                 <span className="clicavel">Cadastre-se</span>
                             </div>
-                            <div className="menu">
+                            <div className={`menu ${this.state.menuAberto ? 'aberto' : ''}`}>
                                 <ul>
                                     <li><span className="clicavel">Sapatos</span></li>
                                     <li><span className="clicavel">Bolsas</span></li>
@@ -46,12 +58,17 @@ class Header extends React.Component{
                                 </ul>
                             </div>
                             <div className="buscaCarrinho">
+                                <Barras className="controleNav" onClick={() => this.alternarMenu()}/>
+                                <div className="logoMobile">
+                                    <img src={logo} alt="original.io"/>
+                                </div>
                                 <label className="busca">
-                                    <img src={lupa} alt="lupa"/>
-                                    <input placeholder="Busca"/>
+                                    <img src={lupa} alt="lupa" onClick={() => this.alternarBusca()}/>
+                                    <input placeholder="Busca" className={`${this.state.buscaAberta ? 'aberto' : ''}`}/>
                                 </label>
                                 <div className="carrinho clicavel" onClick={() => this.abrirModal()}>
                                     <img src={carrinho} alt="sacola"/>
+                                    <img src={carrinhoMobile}  alt="sacola"/>
                                     {this.props.quantidade}
                                 </div>
                                 <Carrinho
